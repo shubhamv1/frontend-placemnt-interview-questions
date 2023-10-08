833141f29e64a49d8c8fe4b84f72565be252b42c
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-08 15:51:56 +0530

    Update Selfcounter .js

--- Selfcounter .js
+++ Selfcounter .js
@@ -1,1 +1,23 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+
+interface Props {
+  name: string;
+}
+
+export default ({ name }: Props) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+     setCounter(counter + 1);
+   }, 1000);
+
+   return () => {
+      clearInterval(interval);
+   };
+  });
+
+  return <h1>{counter}</h1>;
+};
 
