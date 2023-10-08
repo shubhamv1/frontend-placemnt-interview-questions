13a7b722279cca7be25be2319a25b36ef7436533
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-08 15:31:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from "react";
 import './App.css';
 
+const initialTodos = [];
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: action.payload.text,
+          completed: false,
+        },
+      ];
+
+    case "TOGGLE_TODO":
+      return state.map(todo =>
+        todo.id === action.payload.id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+
+    case "CLEAR_COMPLETED":
+      return state.filter(todo => !todo.completed);
+
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [todos, dispatch] = useReducer(todoReducer, initialTodos);
+  const [newTodo, setNewTodo] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newTodo.trim()) {
+      dispatch({
+        type: "ADD_TODO",
+        payload: { text: newTodo },
+      });
+      setNewTodo("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() =>
+                dispatch({ type: "TOGGLE_TODO", payload: { id: todo.id } })
+              }
+            />
+            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+              {todo.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => dispatch({ type: "CLEAR_COMPLETED" })}>
+        Clear Completed
+      </button>
     </div>
   );
 }
 
-export default App;
+export default App;
