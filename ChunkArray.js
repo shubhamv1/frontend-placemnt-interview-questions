aa248a4f79611a37b9f0937d6bb079f0fad7a815
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-08 16:13:07 +0530

    Update ChunkArray.js

--- ChunkArray.js
+++ ChunkArray.js
@@ -1,1 +1,11 @@
-
+function chunk(arr, chunkSize) {
+  const chunks = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = [];
+    for (let j = i; j < Math.min(i + chunkSize, arr.length); j++) {
+      chunk.push(arr[j]);
+    }
+    chunks.push(chunk);
+  }
+  return chunks;
+}
