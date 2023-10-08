1c5e169da4d9c9616bce4f1cc8eb3b0fd32cda81
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-08 16:10:30 +0530

    Update CounterUsereducer.js

--- CounterUsereducer.js
+++ CounterUsereducer.js
@@ -1,1 +1,27 @@
 
+import { useReducer } from "react"
+const initialstate =0;
+const reducer=(state,action)=>{
+  switch(action){
+    case "Increment" :
+      return state+1
+      case "Decrement" :
+        return state-1
+        default :
+        return state
+  }
+}
+
+
+function App() {
+  const [count, dispatch]=useReducer(reducer,initialstate)
+  return (
+    <div className="App">
+    <div>{count}</div>
+      <button onClick={()=>dispatch("Increment")}>Inc</button>
+      <button onClick={()=>dispatch("Decrement")}>Dec</button>
+    </div>
+  )
+}
+
+export default App
