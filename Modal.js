6e49d88495bcc2b258ad988c46bf82d593290aa3
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-09 13:13:46 +0530

    Update Modal.js

--- Modal.js
+++ Modal.js
@@ -1,1 +1,19 @@
 
+import React from "react";
+import "./styles.css";
+import Modal from "react-modal";
+
+
+
+export default function App() {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <div className="App">
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      <Modal isOpen={modalIsOpen}>
+        <button onClick={() => setIsOpen(false)}>Close Modal</button>
+      </Modal>
+    </div>
+  );
+}
