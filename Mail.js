da0d4bf636caa62dcdb179d32c55fff36f05e6e4
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-09 12:31:26 +0530

    Update Mail.js

--- Mail.js
+++ Mail.js
@@ -1,1 +1,14 @@
+function obfuscateEmail(userEmail, num) {
+    let arr = userEmail.split("@");
+    let firstPart = arr[0].slice(0, num);
+    let secondPart = arr[1];
+    let ans = firstPart + "..." + "@" + secondPart;
+    return ans;
+}
+
+// Example usage
+let userEmail = "example@example.com";
+let num = 3;
+let result = obfuscateEmail(userEmail, num);
+console.log(result); 
 
