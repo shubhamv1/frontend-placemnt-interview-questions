aba933778f4921639021618c61e8f754702b9639
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-09 12:12:33 +0530

    Update ErrorBoundry.js

--- ErrorBoundry.js
+++ ErrorBoundry.js
@@ -1,1 +1,45 @@
+// App.js
 
+import "./styles.css";
+import  ErrorBoundary from "./ErrorBoundary"
+function App(){
+  return(
+<ErrorBoundary>
+<Alert message=""></Alert>
+</ErrorBoundary>
+    
+  )
+  
+  }
+  function Alert({message}){
+    if(typeof(message)!=='string'||!message){
+      throw new Error('Message Must Be String')
+
+    }
+    return(
+      <h1>{message}</h1>
+    )
+  }
+  export default App;
+
+
+// ErrorBoundry.js 
+
+import React from "react";
+class ErrorBoundary extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={hasError:false}
+  }
+  componentDidCatch(error){
+    this.setState({hasError:  true})
+  }
+  render(){
+    if(this.state.hasError){
+      return<h1>Some Eror Occured</h1>
+
+    }
+ return this.props.children;
+  }
+}
+export default ErrorBoundary;
