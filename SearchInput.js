9a6d87b80979a16f837a2563e4974b2073e0ada1
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-09 13:39:52 +0530

    Update SearchInput.js

--- SearchInput.js
+++ SearchInput.js
@@ -1,1 +1,47 @@
+import React, { useState } from "react";
+
+const SearchInput = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const debounce = (func, delay) => {
+    let timeoutId;
+    return function (...args) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  };
+
+  const handleSearch = debounce((searchTerm) => {
+ 
+    const items = ["Apple", "Banana", "Orange", "Pineapple", "Mango"];
+    const filteredResults = items.filter(item =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(filteredResults);
+  }, 300); // Debounce delay of 300 milliseconds
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+    handleSearch(value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={searchTerm} onChange={handleChange} />
+      <ul>
+        {searchResults.map((result, index) => (
+          <li key={index}>{result}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchInput;
 
