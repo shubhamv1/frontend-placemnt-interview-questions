d3feeaf9b415f29bc688280cc48a9e99893b3fa6
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-10 13:09:19 +0530

    Update Valid Paranathesis.js

--- Valid Paranathesis.js
+++ Valid Paranathesis.js
@@ -1,1 +1,25 @@
 
+var isValid = function(s) {
+    if(!s || s.length <= 1){
+        return false;
+    }
+    
+    let stack = [];
+    let map = {
+        '(':')',
+        '[':']',
+        '{':'}',
+    }
+    
+    for(let i=0;i< s.length;i++){
+        if(map[s[i]]){
+            stack.push(map[s[i]])
+        }else{
+            if(stack.pop() != s[i]){
+               return false
+            }
+        }
+    }
+    
+    return stack.length > 0 ? false : true;
+};
