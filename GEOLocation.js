97e6c3c5247c751b03b269217c43ab5c115103b6
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-10 14:25:05 +0530

    Update GEOLocation.js

--- GEOLocation.js
+++ GEOLocation.js
@@ -1,1 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+const Location = () => {
+  const [location, setLocation] = useState(null);
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((res) =>{
+      console.log(res);
+      setLocation({long: res.coords.longitude, lat: res.coords.latitude});
+    })
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  return (
+    <div>
+      <button onClick={getCurrentLocation}>Update Location</button>
+      {location && <h1>{`Latitude: ${location.lat} Longitude: ${location.long}`}</h1>}
+    </div>
+  );
+};
+
+export default Location;
 
