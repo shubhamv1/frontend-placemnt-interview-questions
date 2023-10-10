3f6d28006db3ffa7039b960a0317b77f109ff81e
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-10 14:19:52 +0530

    Update Compare2Object.js

--- Copare2Object.js
+++ Copare2Object.js
@@ -1,1 +1,19 @@
+// Define the first object
+let obj1 = {
+//Define first object
+  brand: "Tesla",
+  model: "X",
+  category: "dual-motor",
+  price: "$98490"
+}
 
+
+function check(obj1, obj2) {
+  for(let key in obj2){   //iterate through each keys in obj2
+    if(!obj1[key]) return false;   //check if key exists in obj1
+    else{
+      if(obj1[key] !== obj2[key]) return false;   //check if the values are same
+    }
+  }
+  return true;
+}
