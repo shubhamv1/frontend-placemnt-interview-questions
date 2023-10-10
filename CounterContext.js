00d95a3804608a3788d594eb4c88e1f3d7d4963c
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-10-10 13:05:47 +0530

    Update CounterContext.js

--- CounterContext.js
+++ CounterContext.js
@@ -1,1 +1,50 @@
 
+import "./styles.css";
+
+import React, { createContext, useContext, useState } from "react";
+
+const CounterContext = createContext();
+
+function CounterProvider({ children }) {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <CounterContext.Provider value={{ counter, increment, decrement }}>
+      {children}
+    </CounterContext.Provider>
+  );
+}
+function Counter() {
+  const { counter, increment, decrement } = useContext(CounterContext);
+
+  return (
+    <div>
+      <h2>Counter: {counter}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <CounterProvider>
+      <Counter />
+    </CounterProvider>
+  );
+}
+
+export default App;
+// Create a simple counter application
+//  using React and useContext.
+//  The application should display
+//  a counter and two buttons, one
+//  to increment the counter and one to decrement it. The counter value should be stored in a context object, and the buttons should use the useContext hook to access and update the counter value.
